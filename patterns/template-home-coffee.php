130b0e2c9b6531d45edc2ee74f396d5a6935d62b
Edward-Kabue <ekabue0@gmail.com>
2025-04-24 23:09:20 +0300

    modified:   css/dev/global.css

---
+++ patterns/template-home-coffee.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Coffee Shop Home Template
+ * Slug: twentytwentyfour/template-home-coffee
+ * Template Types: front-page, home
+ * Viewport width: 1400
+ * Inserter: no
+ */
+?>
+
+<!-- wp:template-part {"slug":"header","area":"header","tagName":"header"} /-->
+
+<!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"0","margin":{"top":"0"}}},"layout":{"type":"default"}} -->
+<main class="wp-block-group" style="margin-top:0">
+  <!-- wp:pattern {"slug":"twentytwentyfour/hero-coffee"} /-->
+  
+  <!-- wp:post-content {"layout":{"inherit":true}} /-->
+</main>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"footer","area":"footer","tagName":"footer"} /-->
